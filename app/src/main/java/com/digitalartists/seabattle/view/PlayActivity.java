5a345cbe0c63ac4dffd71cd690fe7d6226495e3f
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2023-08-03 20:22:22 +0300

    Almost working Ship placement window

--- app/src/main/java/com/digitalartists/seabattle/view/PlayActivity.java
+++ app/src/main/java/com/digitalartists/seabattle/view/PlayActivity.java
@@ -28,6 +28,7 @@ public class PlayActivity extends AppCompatActivity {
     private Settings settings;          // settings object
     private int[] mines_arr = null;     // array of mines
     private int[] visited_arr = null;   // cols*rows array of each button id
+    private int[] clicked_cells_arr = null;
 
     private int selectedTypeOfObject;
     private int onePartShipsNum;
@@ -35,7 +36,9 @@ public class PlayActivity extends AppCompatActivity {
     private int threePartShipsNum;
     private int minesNum;
 
-    @SuppressLint("DefaultLocale")
+    private int clicksInARow;
+
+    @SuppressLint({"DefaultLocale", "ResourceType"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +60,7 @@ public class PlayActivity extends AppCompatActivity {
         threePartShipsNum = 2;
         minesNum = 4;
 
+        clicksInARow = 0;
         selectedTypeOfObject = -1;
 
         TextView textViewOnePartShipsNum = findViewById(R.id.numberOfOnePartShips_id);
@@ -158,7 +162,7 @@ public class PlayActivity extends AppCompatActivity {
                 // handler for CLICK on this image (button)
                 imageButton.setOnClickListener(v -> {
                     ImageView iView = (ImageView) v;
-                    Toast.makeText(this, "You clicked button=" + v.getId(), Toast.LENGTH_LONG).show();
+                    //Toast.makeText(this, "You clicked button=" + v.getId(), Toast.LENGTH_LONG).show();
                     if (selectedTypeOfObject == 0 && visited_arr[iView.getId()] == 0 && minesNum > 0) {
                         iView.setImageResource(R.drawable.mine_usual);
                         visited_arr[iView.getId()] = 10;
@@ -167,7 +171,202 @@ public class PlayActivity extends AppCompatActivity {
                         return;
                     }
 
+                    if (selectedTypeOfObject == 1 && visited_arr[iView.getId()] == 0 && onePartShipsNum > 0) {
+                        setIconToButton(iView, 1);
+                        visited_arr[iView.getId()] = 1;
+                        onePartShipsNum--;
+                        clicksInARow = 0;
+                        textViewOnePartShipsNum.setText(""+onePartShipsNum);
+                        int id = iView.getId();
+                        if (checkIfValidCoord(id, id - 1) && visited_arr[id-1] == 0) {
+                            ImageView imageView = findViewById(id - 1);
+                            visited_arr[id-1] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id - 1 - 10) && visited_arr[id-1-10] == 0) {
+                            ImageView imageView = findViewById(id - 1 - 10);
+                            visited_arr[id-1-10] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id - 10) && visited_arr[id-10] == 0) {
+                            ImageView imageView = findViewById(id - 10);
+                            visited_arr[id-10] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id - 10 + 1) && visited_arr[id-10+1] == 0) {
+                            ImageView imageView = findViewById(id - 10 + 1);
+                            visited_arr[id-10+1] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id + 1) && visited_arr[id+1] == 0) {
+                            ImageView imageView = findViewById(id + 1);
+                            visited_arr[id+1] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id + 1 + 10) && visited_arr[id+1+10] == 0) {
+                            ImageView imageView = findViewById(id + 1 + 10);
+                            visited_arr[id+1+10] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id + 10) && visited_arr[id+10] == 0) {
+                            ImageView imageView = findViewById(id + 10);
+                            visited_arr[id+10] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                        if (checkIfValidCoord(id, id - 1 + 10) && visited_arr[id-1+10] == 0) {
+                            ImageView imageView = findViewById(id - 1 + 10);
+                            visited_arr[id-1+10] = 5;
+                            setIconToButton(imageView, 5);
+                        }
+                    }
+
+                    if (selectedTypeOfObject == 2 && visited_arr[iView.getId()] == 0 && twoPartShipsNum > 0) {
+                        if (clicksInARow <= 0) {
+                            clicked_cells_arr = new int[2];
+                            buttonMines.setEnabled(false);
+                            buttonOnePartShips.setEnabled(false);
+                            buttonThreePartShips.setEnabled(false);
+                            clicked_cells_arr[0] = iView.getId();
+                            visited_arr[iView.getId()] = 2;
+                            clicksInARow++;
+                            setIconToButton(iView, 2);
+                        } else {
+                            clicksInARow = 0;
+                            clicked_cells_arr[1] = iView.getId();
+                            visited_arr[iView.getId()] = 2;
+                            buttonMines.setEnabled(true);
+                            buttonOnePartShips.setEnabled(true);
+                            buttonThreePartShips.setEnabled(true);
+                            setIconToButton(iView, 2);
+
+                            for (int id : clicked_cells_arr) {
+                                if (checkIfValidCoord(id, id - 1) && visited_arr[id - 1] == 0) {
+                                    ImageView imageView = findViewById(id - 1);
+                                    visited_arr[id - 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 1 - 10) && visited_arr[id - 1 - 10] == 0) {
+                                    ImageView imageView = findViewById(id - 1 - 10);
+                                    visited_arr[id - 1 - 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 10) && visited_arr[id - 10] == 0) {
+                                    ImageView imageView = findViewById(id - 10);
+                                    visited_arr[id - 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 10 + 1) && visited_arr[id - 10 + 1] == 0) {
+                                    ImageView imageView = findViewById(id - 10 + 1);
+                                    visited_arr[id - 10 + 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 1) && visited_arr[id + 1] == 0) {
+                                    ImageView imageView = findViewById(id + 1);
+                                    visited_arr[id + 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 1 + 10) && visited_arr[id + 1 + 10] == 0) {
+                                    ImageView imageView = findViewById(id + 1 + 10);
+                                    visited_arr[id + 1 + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 10) && visited_arr[id + 10] == 0) {
+                                    ImageView imageView = findViewById(id + 10);
+                                    visited_arr[id + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 1 + 10) && visited_arr[id - 1 + 10] == 0) {
+                                    ImageView imageView = findViewById(id - 1 + 10);
+                                    visited_arr[id - 1 + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                            }
+
+                            twoPartShipsNum--;
+                            textViewTwoPartShipsNum.setText(""+ twoPartShipsNum);
+
+
+                        }
+
+                    }
+
+                    if (selectedTypeOfObject == 3 && visited_arr[iView.getId()] == 0 && threePartShipsNum > 0) {
+                        if (clicksInARow <= 0) {
+                            clicked_cells_arr = new int[3];
+                            buttonMines.setEnabled(false);
+                            buttonOnePartShips.setEnabled(false);
+                            buttonTwoPartShips.setEnabled(false);
+                            clicked_cells_arr[0] = iView.getId();
+                            visited_arr[iView.getId()] = 3;
+                            clicksInARow++;
+                            setIconToButton(iView, 3);
+                        } else if (clicksInARow == 1) {
+                            clicked_cells_arr[1] = iView.getId();
+                            visited_arr[iView.getId()] = 3;
+                            clicksInARow++;
+                            setIconToButton(iView, 3);
+                        } else {
+                            clicksInARow = 0;
+                            clicked_cells_arr[2] = iView.getId();
+                            visited_arr[iView.getId()] = 3;
+                            buttonMines.setEnabled(true);
+                            buttonOnePartShips.setEnabled(true);
+                            buttonTwoPartShips.setEnabled(true);
+                            setIconToButton(iView, 3);
+
+                            for (int id : clicked_cells_arr) {
+                                if (checkIfValidCoord(id, id - 1) && visited_arr[id - 1] == 0) {
+                                    ImageView imageView = findViewById(id - 1);
+                                    visited_arr[id - 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 1 - 10) && visited_arr[id - 1 - 10] == 0) {
+                                    ImageView imageView = findViewById(id - 1 - 10);
+                                    visited_arr[id - 1 - 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 10) && visited_arr[id - 10] == 0) {
+                                    ImageView imageView = findViewById(id - 10);
+                                    visited_arr[id - 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 10 + 1) && visited_arr[id - 10 + 1] == 0) {
+                                    ImageView imageView = findViewById(id - 10 + 1);
+                                    visited_arr[id - 10 + 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 1) && visited_arr[id + 1] == 0) {
+                                    ImageView imageView = findViewById(id + 1);
+                                    visited_arr[id + 1] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 1 + 10) && visited_arr[id + 1 + 10] == 0) {
+                                    ImageView imageView = findViewById(id + 1 + 10);
+                                    visited_arr[id + 1 + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id + 10) && visited_arr[id + 10] == 0) {
+                                    ImageView imageView = findViewById(id + 10);
+                                    visited_arr[id + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                                if (checkIfValidCoord(id, id - 1 + 10) && visited_arr[id - 1 + 10] == 0) {
+                                    ImageView imageView = findViewById(id - 1 + 10);
+                                    visited_arr[id - 1 + 10] = 5;
+                                    setIconToButton(imageView, 5);
+                                }
+                            }
+
+                            threePartShipsNum--;
+                            textViewThreePartShipsNum.setText("" + threePartShipsNum);
+
+
+                        }
+                    }
+
                 });
+
+
 
                 tableRow.addView(imageButton);
             }
@@ -178,40 +377,39 @@ public class PlayActivity extends AppCompatActivity {
 
     }
 
+    // check if coordinates of button are valid
+    boolean checkIfValidCoord(int firstParam, int num) {
+
+        // transform num into x and y coordinates
+        int x = num % 10;
+        int y = num / 10;
+        int x_prev = firstParam % 10;
+
+        // check if cell is out of range
+        return x >= 0 && y >= 0 && x < 10 && y < 10 &&
+                (x != 0 || x_prev != (10 - 1)) && (x != (10 - 1) || x_prev != 0);
+    }
+
 
     // set icon to button in order to show additional information needed for user
-    void setIconToButton(ImageView imageView, int minesNum) {
+    void setIconToButton(ImageView imageView, int type) {
 
         if (imageView == null) {
             Log.d("setIconToButton", "Button was not found!");
             return;
         }
 
-        /*if (minesNum == 0) {
-            imageView.setImageResource(R.drawable.empty_cell);
-        } else if (minesNum == 1) {
+        if (type == 1) {
             imageView.setImageResource(R.drawable.digit_1);
-        } else if (minesNum == 2) {
+        } else if (type == 2) {
             imageView.setImageResource(R.drawable.digit_2);
-        } else if (minesNum == 3) {
+        } else if (type == 3) {
             imageView.setImageResource(R.drawable.digit_3);
-        } else if (minesNum == 4) {
-            imageView.setImageResource(R.drawable.digit_4);
-        } else if (minesNum == 5) {
+        } else if (type == 5) {
             imageView.setImageResource(R.drawable.digit_5);
-        } else if (minesNum == 6) {
-            imageView.setImageResource(R.drawable.digit_6);
-        } else if (minesNum == 7) {
-            imageView.setImageResource(R.drawable.digit_7);
-        } else if (minesNum == 8) {
-            imageView.setImageResource(R.drawable.digit_8);
-        } else if (minesNum == -1) {
-            imageView.setImageResource(R.drawable.flag);
-        } else if (minesNum == -2) {
-            imageView.setImageResource(R.drawable.non_clicked_cell);
-        } else if(minesNum == -3) {
-            imageView.setImageResource(R.drawable.mine_clicked);
-        }*/
+        } else if (type == 10) {
+            imageView.setImageResource(R.drawable.mine_usual);
+        }
 
     }
 
